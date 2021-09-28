093fe835f9e0463ecc9d50d6aab48411bf8c4632
ayushmgupta <ayushgupta7000@gmail.com>
2021-09-28 16:58:19 +0530

    Revert "this is our thirdv1 commit"

--- app/src/main/java/com/amg/retrofit_api/MainActivity.java
+++ app/src/main/java/com/amg/retrofit_api/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("this is the change for the 2nd commit");
-        this is the change for the 3rd commit
+        println("this is the change for the 3nd commit");
     }
 }
